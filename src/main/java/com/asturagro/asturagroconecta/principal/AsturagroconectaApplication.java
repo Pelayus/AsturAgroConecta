8dd9b755e5d1e4b30ae0d1db8c9813acc5ab2160
Pelayus <rodriguezalvarezpelayo30@gmail.com>
2025-07-26 13:43:15 +0200

    Subida inicial del proyecto Spring Boot

---
+++ src/main/java/com/asturagro/asturagroconecta/principal/AsturagroconectaApplication.java
@@ -0,0 +1,13 @@
+package com.asturagro.asturagroconecta.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsturagroconectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsturagroconectaApplication.class, args);
+	}
+
+}
